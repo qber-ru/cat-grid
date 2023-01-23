88565ff28f89a33ae93e669e83cabb8df11852a4
QBER <qbr95@vk.com>
2023-01-23 21:58:57 +0700

    cat grid

---
+++ scripts/index.js
@@ -0,0 +1,107 @@
+let main = document.querySelector("main");
+const imgCat = 'imgs/cat.jpg'
+const updCards = function (data) {
+    main.innerHTML = "";
+    data.forEach(function (cat) {
+        if (cat.id) {
+            let card = `<div class="${cat.favourite ? "card like" : "card"}" style="background-image:
+    url(${cat.img_link || imgCat})">
+    <span>${cat.name}</span>
+    </div>`;
+            main.innerHTML += card;
+        }
+    });
+    let cards = document.getElementsByClassName("card");
+    for (let i = 0, cnt = cards.length; i < cnt; i++) {
+        const width = cards[i].offsetWidth;
+        cards[i].style.height = width * 0.6 + "px";
+    }
+}
+
+
+const api = new Api("mi-gl");
+
+
+let form = document.forms[0];
+form.img_link.addEventListener("change", (e) => {
+    form.firstElementChild.style.backgroundImage = `url(${e.target.value})`
+})
+form.img_link.addEventListener("input", (e) => {
+
+    form.firstElementChild.style.backgroundImage = `url(${e.target.value})`
+})
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let body = {};
+    for (let i = 0; i < form.elements.length; i++) {
+        let inp = form.elements[i];
+        if (inp.type === "checkbox") {
+            body[inp.name] = inp.checked;
+        } else if (inp.name && inp.value) {
+            if (inp.type === "number") {
+                body[inp.name] = +inp.value;
+            } else {
+                body[inp.name] = inp.value;
+            }
+        }
+    }
+    console.log(body);
+    api.addCat(body)
+        .then(res => res.json())
+        .then(data => {
+            if (data.message === "ok") {
+                form.reset();
+                closePopupForm.click();
+                api.getCat(body.id)
+                    .then(res => res.json())
+                    .then(cat => {
+                        if (cat.message === "ok") {
+                            catsData.push(cat.data);
+                            localStorage.setItem("cats", JSON.stringify(catsData));
+
+                            getCats(api, catsData);
+                        } else {
+                            console.log(cat);
+                        }
+                    })
+            } else {
+                console.log(data);
+                api.getIds().then(r => r.json()).then(d => console.log(d));
+            }
+        })
+})
+
+let catsData = localStorage.getItem("cats");
+catsData = catsData ? JSON.parse(catsData) : [];
+const getCats = function (api, store) {
+    if (!store.length) {
+        api.getCats()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.message === "ok") {
+                    localStorage.setItem("cats", JSON.stringify(data.data));
+                    catsData = [...data.data];
+                    updCards(data.data);
+                }
+            })
+    } else {
+        updCards(store);
+    }
+}
+getCats(api, catsData);
+
+let addBtn = document.querySelector("#add");
+let popupForm = document.querySelector("#popup-form");
+let closePopupForm = popupForm.querySelector(".popup-close");
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!popupForm.classList.contains("active")) {
+        popupForm.classList.add("active");
+        popupForm.parentElement.classList.add("active");
+    }
+});
+closePopupForm.addEventListener("click", () => {
+    popupForm.classList.remove("active");
+    popupForm.parentElement.classList.remove("active");
+})
